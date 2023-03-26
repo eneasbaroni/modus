02b16e5f224e061401c0e92c8851a0d1de7deb1c
Eneas Baroni <eneasbaroni@gmail.com>
2023-03-26 17:30:13 -0300

    scroll page

--- src/pages/Gestion_Publica/GestionPublica.js
+++ src/pages/Gestion_Publica/GestionPublica.js
@@ -3,8 +3,19 @@ import Cards from './components/Cards'
 import Form from './components/Form'
 import Why from './components/Why'
 import './GestionPublica.css'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const GestionPublica = () => {
+
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+
+
   return (
     <main id='gestionPublica'>      
       <Cards/> 
