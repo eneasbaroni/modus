57de430d3039a941a88e0e9c8c8ec7205221affb
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-03-27 10:33:29 -0300

    Loader

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Loader from '../../components/Loader/Loader'
 import Actions from './Actions'
 import Cards from './Cards'
 import Form from './Form'
@@ -9,6 +10,7 @@ import Style from './Style'
 
 const Home = () => {
   /* const [scrollY, setScrollY] = useState(0) */
+  const [loading, setLoading] = useState(true)
   const [position, setPosition] = useState('0%')
 
   const windowHeight = window.innerHeight;
@@ -32,24 +34,34 @@ const Home = () => {
 
   return (
     <main id='home'>
-      <div className='titles-bg'>
-        {/* <div className='medidor'></div> */}
-        <section className='homeBannerContainer'>
-          <div className='mainImgContainer'>     
-            <img className='mainImage' src='./images/main/main_image.png' alt='main' />        
-            <img className='mainImage2' src='./images/main/main_image 2.png' alt='main' style={{objectPosition: position }}/>
-          </div> 
-          <div className='mainTitleContainer'>        
-            <h1><span>Gestionamos</span> la<br/>conversación de<br/>tu marca con equipos<br/>especializados de<br/>respuesta</h1>
-            <h2 className='mainTextMask'><span>Gestionamos</span> la<br/>conversación de<br/>tu marca con equipos<br/>especializados de<br/>respuesta</h2>
-          </div>     
-        </section>
-        <div className='mainBtnContainer'>
-          <a className='mainBtn' href='#actions'>¿QUÉ NECESITÁS?</a>
-          <img className='mainBtnArrow' src='./images/main/main_arrow.svg' alt='arrow' />
-        </div>
 
-      <Actions/>
+      <img  src='./images/main/main_image.png' alt='main' style={{display: 'none'}}/>        
+      <img  src='./images/main/main_image 2.png' alt='main' style={{display: 'none'}} onLoad={() => setLoading(false)}/>
+
+      <div className='titles-bg'>
+      {loading
+        ? <Loader/>
+        :
+          <>
+            {/* <div className='medidor'></div> */}
+            <section className='homeBannerContainer'>
+              <div className='mainImgContainer'>     
+                <img className='mainImage' src='./images/main/main_image.png' alt='main' />        
+                <img className='mainImage2' src='./images/main/main_image 2.png' alt='main' style={{objectPosition: position }}/>
+              </div> 
+              <div className='mainTitleContainer'>        
+                <h1><span>Gestionamos</span> la<br/>conversación de<br/>tu marca con equipos<br/>especializados de<br/>respuesta</h1>
+                <h2 className='mainTextMask'><span>Gestionamos</span> la<br/>conversación de<br/>tu marca con equipos<br/>especializados de<br/>respuesta</h2>
+              </div>     
+            </section>
+            <div className='mainBtnContainer'>
+              <a className='mainBtn' href='#actions'>¿QUÉ NECESITÁS?</a>
+              <img className='mainBtnArrow' src='./images/main/main_arrow.svg' alt='arrow' />
+            </div>
+            <Actions/>
+          </>
+          }
+
       </div>
       <Style/>
       <Cards/>
