a97fc8122f0bdc32fe4cd298b0b747b51a440b9e
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-03-27 10:19:25 -0300

    Loader

--- src/pages/Gestion_Publica/components/Cards.js
+++ src/pages/Gestion_Publica/components/Cards.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import Loader from "../../../components/Loader/Loader"
 import './Cards.css'
 
 
 const Cards = () => {
+  const [loading, setLoading] = useState(true)
   const [opacity, setOpacity] = useState(1)
   const [cardActive, setCardActive] = useState('card-01')
   const [imageActive, setImageActive] = useState('card-01')
@@ -14,28 +16,36 @@ const Cards = () => {
       setImageActive(card)
       setOpacity(1)
     }, 500)
-  }
-
-
-
+  } 
 
   return (
     <section className='gpCardsContainer'>
 
-        <div className="cardImgContainer">
-          <div className='noise'></div>
-          <img className='gpCardImgContainer' src='./images/gestion_publica/image-container.png' alt='card' />
-          {imageActive === 'card-01' && <img className='gpCardImg' src='./images/gestion_publica/card-01.png' alt='card' style={{scale: opacity}}/>}
-          {imageActive === 'card-02' && <img className='gpCardImg' src='./images/gestion_publica/card-02.png' alt='card' style={{scale: opacity}}/>}
-          {imageActive === 'card-03' && <img className='gpCardImg' src='./images/gestion_publica/card-03.png' alt='card' style={{scale: opacity}}/>}
-          {imageActive === 'card-04' && <img className='gpCardImg' src='./images/gestion_publica/card-04.png' alt='card' style={{scale: opacity}}/>}
-          {imageActive === 'card-05' && <img className='gpCardImg' src='./images/gestion_publica/card-05.png' alt='card' style={{scale: opacity}}/>}
-          
-          
-          
-          <img className='cardCircle' src='./images/gestion_publica/card-circle.png' alt='card' />
-        </div>
+      <img className='gpCardImg' src='./images/gestion_publica/card-01.png' alt='card' style={{display: 'none'}}/>
+      <img className='gpCardImg' src='./images/gestion_publica/card-02.png' alt='card' style={{display: 'none'}}/>
+      <img className='gpCardImg' src='./images/gestion_publica/card-03.png' alt='card' style={{display: 'none'}}/>
+      <img className='gpCardImg' src='./images/gestion_publica/card-04.png' alt='card' style={{display: 'none'}}/>
+      <img className='gpCardImg' src='./images/gestion_publica/card-04.png' alt='card' style={{display: 'none'}} onLoad={() => setLoading(false)}/>
+
+
+
+      {loading && <Loader />}
+      
+
+      <div className="cardImgContainer">
+        <div className='noise'></div>
+        <img className='gpCardImgContainer' src='./images/gestion_publica/image-container.png' alt='card' />
+        {imageActive === 'card-01' && <img className='gpCardImg' src='./images/gestion_publica/card-01.png' alt='card' style={{scale: opacity}}/>}
+        {imageActive === 'card-02' && <img className='gpCardImg' src='./images/gestion_publica/card-02.png' alt='card' style={{scale: opacity}}/>}
+        {imageActive === 'card-03' && <img className='gpCardImg' src='./images/gestion_publica/card-03.png' alt='card' style={{scale: opacity}}/>}
+        {imageActive === 'card-04' && <img className='gpCardImg' src='./images/gestion_publica/card-04.png' alt='card' style={{scale: opacity}}/>}
+        {imageActive === 'card-05' && <img className='gpCardImg' src='./images/gestion_publica/card-05.png' alt='card' style={{scale: opacity}}/>}
         
+        
+        
+        <img className='cardCircle' src='./images/gestion_publica/card-circle.png' alt='card' />
+      </div>
+      
       <div className='gpCardInfo'>
         <h1>Gestión <br/>pública</h1> 
         <div className={cardActive === 'card-01' ? 'textGroup activeGroup' : 'textGroup'} id="card-01" onMouseEnter= {() => handleActiveCard('card-01')}>
