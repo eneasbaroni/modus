ea386be9509d498f4144963ec4ca250ac8692483
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-04-04 12:50:39 -0300

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,21 +4,19 @@ import Form from './components/Form'
 import Presentation from './components/Presentation/Presentation'
 import './Home.css'
 import Style from './components/Style' 
+import Header from './components/Header/Header'
 
 
 const Home = () => {
-  
-
 
   return (
     <main id='home'>
-
+      <Header/>
       <Presentation/>
       <Actions/>
       <Style/>
       <Cards/>
       <Form/>
-
     </main>
   )
 }
