d88b79753bafaa55a5ceaf49e06b333f50671be8
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-04-04 12:49:28 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import GestionPublica from './pages/Gestion_Publica/GestionPublica';
 import Home from './pages/Home/Home';
 import SectorPirvado from './pages/Sector_Privado/SectorPirvado';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path='/gestion-publica' element={<GestionPublica/>}/>
-          <Route path='/sector-privado' element={<SectorPirvado/>}/>  
-        </Routes>
+    <BrowserRouter>      
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/gestion-publica' element={<GestionPublica/>}/>
+        <Route path='/sector-privado' element={<SectorPirvado/>}/>  
+      </Routes>
       <Footer/>
     </BrowserRouter>
   );
