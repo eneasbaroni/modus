6887f0394861d5a137d1455072ddbacb6044924a
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2024-01-05 14:52:16 -0300

    update edit form

--- src/pages/Admin/components/ReportViewer/UpdateReport.jsx
+++ src/pages/Admin/components/ReportViewer/UpdateReport.jsx
@@ -9,13 +9,13 @@ const UpdateReport = ({userId, report, closeModal, loadReports}) => {
 	const [reportUpdated, setReportUpdated] = useState({
 		_id: report._id,
 		fecha: report.fecha,
-		nombre: "",
-		informe: "",
-		informeMobile: "",
-		multimedia: "",
+		nombre: report.nombre,
+		informe: report.informe,
+		informeMobile: report.informeMobile,
+		multimedia: report.multimedia,
 		/* estadistica: "", */
-		grafico: "",
-		carpeta: ""
+		grafico: report.grafico,
+		carpeta: report.carpeta || ""
 	})
 
 	const handleInputChange = (event) => {
@@ -60,11 +60,17 @@ const UpdateReport = ({userId, report, closeModal, loadReports}) => {
 			<div className="updateReportContainer">
 				<form className="actionForms" onSubmit={handleSubmit}>
 						<legend>Actualizar Informe</legend> 
-						<input type="text" name="nombre" id="nombre" placeholder='Nombre del Informe' onChange={handleInputChange} required/>				
-						<input type="text" name="informe" id="informe" placeholder='Link a Informe en PDF Desktop' onChange={handleInputChange} required/>				
-						<input type="text" name="informeMobile" id="informeMobile" placeholder='Link a Informe en PDF Mobile' onChange={handleInputChange} required/>
-						<input type="text" name="multimedia" id="Multimedia" placeholder='Link a archivo de Audio' onChange={handleInputChange} required/>
-						<input type="text" name="grafico" id="grafico" placeholder='Link a Grafico' onChange={handleInputChange} required/>
+						<label>Nombre del Informe</label>
+						<input type="text" name="nombre" id="nombre" placeholder='Nombre del Informe' defaultValue={report.nombre} onChange={handleInputChange} required/>				
+						<label>Link a Informe en PDF Desktop</label>
+						<input type="text" name="informe" id="informe" placeholder='Link a Informe en PDF Desktop' defaultValue={report.informe} onChange={handleInputChange} required/>				
+						<label>Link a Informe en PDF Mobile</label>
+						<input type="text" name="informeMobile" id="informeMobile" placeholder='Link a Informe en PDF Mobile' defaultValue={report.informeMobile} onChange={handleInputChange} required/>
+						<label>Link a archivo de Audio</label>
+						<input type="text" name="multimedia" id="Multimedia" placeholder='Link a archivo de Audio' defaultValue={report.multimedia} onChange={handleInputChange} required/>
+						<label>Link a Gráfico</label>
+						<input type="text" name="grafico" id="grafico" placeholder='Link a Gráfico' defaultValue={report.grafico} onChange={handleInputChange} required/>
+						<label>Link a Carpeta compartida</label>
 						<input type="text" name="carpeta" id="carpeta" placeholder='Carpeta compartida (opcional)' onChange={handleInputChange}/>	
 			
 						{ reportUpdated.nombre && reportUpdated.informe && reportUpdated.informeMobile && reportUpdated.multimedia  && reportUpdated.grafico ?			
